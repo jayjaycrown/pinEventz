b63d1e17c078a49596773bcd0580ce9bf9c6d82c
jayjaycrown <jayjaycrown@gmail.com>
2019-12-02 08:13:24 +0100

    updated

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/pinEventz'));
+app.use(express.static(__dirname + '/dist/browser'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/pinEventz/index.html'));
+res.sendFile(path.join(__dirname+'/dist/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
